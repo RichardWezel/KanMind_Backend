936ca78ed16f5af6261a3c9b22d962f3e074fee0
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-06-17 17:13:04 +0200

    created a CustomUserManager and created superuser

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,5 +1,28 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Die E-Mail-Adresse muss angegeben werden.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser muss is_staff=True haben.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser muss is_superuser=True haben.')
+
+        return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     username = None
@@ -7,7 +30,9 @@ class CustomUser(AbstractUser):
     fullname = models.CharField(max_length=150)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []  # oder ['fullname'], falls nötig
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager() 
 
     def __str__(self):
         return self.email
